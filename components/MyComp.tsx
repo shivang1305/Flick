af90a19edfa88458e53a9ce650f32a1da0c21dfc
Shivang Sharma <sharmashivang1999@gmail.com>
2024-07-27 22:53:12 +0530

    chore: expo-router setup completed for navigation

--- components/MyComp.tsx
+++ components/MyComp.tsx
@@ -1,4 +1,5 @@
-import { View, Text } from "react-native";
+import { Link, router } from "expo-router";
+import { View, Text, Button, Pressable } from "react-native";
 import { useTailwind } from "tailwind-rn";
 
 const MyComp = () => {
@@ -6,7 +7,8 @@ const MyComp = () => {
 
   return (
     <View style={tailwind("flex-1 justify-center items-center")}>
-      <Text style={tailwind("text-yellow-500")}>MyComp</Text>
+      <Text style={tailwind("text-orange-500 text-3xl")}>MyComp</Text>
+      <Button onPress={() => router.push("/home")} title="Home Button" />
     </View>
   );
 };
